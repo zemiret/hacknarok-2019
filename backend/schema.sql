01f8e2ef6981d5a0080a573440b0e5807527656f
Szymon Borowy <sborowy4@gmail.com>
2019-04-06 16:13:04 +0200

    AddSchemaOfDB

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,17 +1,33 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS clans;
+DROP TABLE IF EXISTS beacons;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
-);
+CREATE TABLE clans{
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    name VARCHAR(30) NOT NULL;
+    logo VARCHAR(30);
+    color VARCHAR(30);
+}
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
-);
+
+CREATE TABLE users{
+    id INTEGER PRIMARY KEY;
+    username VARCHAR(30) UNIQUE NOT NULL;
+    email VARCHAR(40) UNIQUE NOT NULL;
+    password VARCHAR(20) NOT NULL;
+    clan_id INTEGER;
+    picture VARCHAR(30);
+
+
+    FOREIGN KEY (clan_id) REFERENCES clan (id)
+}
+
+CREATE TABLE beacons{
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    middle_point_x INTEGER NOT NULL;
+    middle_point_y INTEGER NOT NULL;
+    range INTEGER NOT NULL;
+    clan_id INTEGER;
+
+    FOREIGN KEY (clan_id) REFERENCES clan (id)
+}
